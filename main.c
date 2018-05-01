366dd9bd7534e595996e3ccc9f346b71c04dfd32
Jamison Kimminau <thepistochini@gmail.com>
2018-04-30 19:57:43 -0700

    4_30update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 17:12:39 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/04/30 19:52:53 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/04/30 19:57:29 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,5 @@
 
 int		main(void)
 {
-	wchar_t	i;
-	char	j;
-
-	i = 0;
-	j = 0;
-	//while (i < 1000)
-		//printf("%C\t i = %d\t%c\tj = %d\n", i++, i, j++, j);
-	printf("%C\t%d,%C\t%d\n", L'暖', L'暖', L'ح', L'ح');
 	return (0);
 }
