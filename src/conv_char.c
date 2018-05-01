f2e6f294031177cf829483bf3c17498ed91d29fa
Jamison Kimminau <thepistochini@gmail.com>
2018-04-30 20:41:37 -0700

    4_30

--- src/conv_char.c
+++ src/conv_char.c
@@ -6,7 +6,7 @@
 /*   By: jkimmina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/30 19:28:22 by jkimmina          #+#    #+#             */
-/*   Updated: 2018/04/30 19:54:16 by jkimmina         ###   ########.fr       */
+/*   Updated: 2018/04/30 20:41:23 by jkimmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 char	*conv_char(va_list *ap, t_flags *flags)
 {
 	char	*ret;
-	wchar_t	c;
 
 	ret = ft_strnew(2);
-	c = va_arg(*ap, wchar_t);
 	ret[0] = va_arg(*ap, int);
 	ret[1] = '\0';
 	if (ret[0] < 32 || ret[0] > 126)
